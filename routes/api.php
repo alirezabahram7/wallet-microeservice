7a8b75bc4cd59dd9ea7c79df1681f24e36357b33
alireza bahram <alib327@gmail.com>
2023-03-18 21:41:40 +0330

    feat : add basic routes

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/balance', [WalletController::class, 'getBalance']);
+Route::post('/add-money', [WalletController::class, 'addMoney']);
